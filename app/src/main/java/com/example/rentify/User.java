f04bfa192ba125b45496b31882eb6a1ff5971858
amybradbrook <amy.bradbrook@live.ca>
2024-09-29 19:13:28 -0400

    Updated User class to include getters for email, username, and name, as well as variables for names.

--- app/src/main/java/com/example/rentify/User.java
+++ app/src/main/java/com/example/rentify/User.java
@@ -4,19 +4,37 @@ public abstract class User {
 
     private String userID;
     private String accountType;
+    private String firstName;
+    private String lastName;
 
     /**
      * Constructor
      * @param userID String representing UserID in firebase
      * @param accountType can be Admin, Lessor, or Rentor
      */
-    public User(String userID, String accountType){
+    public User(String userID, String accountType, String firstName, String lastName){
         this.userID = userID;
         this.accountType = accountType;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getUserType(){
         return this.accountType;
+    }
+
+    public String getFirstName(){return this.firstName;}
+
+    public String getLastName(){return this.lastName;}
+
+    public String getEmail(String userID){
+        //Make call to database and return email
+        return "";
+    }
+
+    public String getUsername(String userID){
+        //make call to database and return username
+        return "";
     }
 
 }
