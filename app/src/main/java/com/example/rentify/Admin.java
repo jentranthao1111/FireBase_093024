4e4cd3999161326c2d3fcb93c03e2733b9127964
amybradbrook <amy.bradbrook@live.ca>
2024-09-29 19:39:25 -0400

    Updated User to include enums for class role.

--- app/src/main/java/com/example/rentify/Admin.java
+++ app/src/main/java/com/example/rentify/Admin.java
@@ -4,11 +4,21 @@ import java.util.List;
 
 public class Admin extends User{
 
+    /**
+     * Constructor.
+     * @param userID
+     * @param firstName
+     * @param lastName
+     */
     public Admin(String userID, String firstName, String lastName){
-        super(userID, "Admin", firstName, lastName);
+        super(userID, "admin", firstName, lastName);
 
     }
 
+    /**
+     * Returns list of users, found from firebase.
+     * @return user list.
+     */
     public List<User> LoadAllUsers(){
         //makes a call to firebase to get a list of users. Find a way to feed to welcome page
         List<User> listOfUsers = null;
